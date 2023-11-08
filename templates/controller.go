94e752e4cfdf05a0ddc209e66788f3c59f68570b
Lucas Oliveira <lucasnhso@gmail.com>
2023-11-08 12:39:16 -0300

    change templates from .tmpl to .go files

---
+++ templates/controller.go
@@ -0,0 +1,15 @@
+package templates
+
+func Controller() string {
+	return `
+    import {{.PCaseName}}UseCase from "../useCases/{{.PCaseName}}UseCase";
+    import ResourceController from "sygnus";
+    
+    export default class {{.PCaseName}}Controller extends ResourceController {
+      constructor() {
+        super(new {{.PCaseName}}UseCase());
+      }
+    }
+  
+  `
+}
---
+++ templates/repository.go
@@ -0,0 +1,15 @@
+package templates
+
+func Repository() string {
+	return `
+    import { PrismaClient } from "@prisma/client";
+    import ResourceRepository from "sygnus";
+    import prismaClient from "../database/prismaClient";
+    
+    export default class {{.PCaseName}}Repository extends BaseRepository {
+      constructor() {
+        super(prismaClient, "{{.Name}}"); 
+      } 
+    }
+  `
+}
